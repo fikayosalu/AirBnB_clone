b74066911f5de34a807dd7f62722a0ebb1663001
fikayosalu <salufikayo@gmail.com>
2024-11-13 08:35:44 +0100

    Creates the BaseModel class for task 3

---
+++ AirBnB_clone_console/models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""base_model module"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        """
+        Initializes instances of the class with attributes
+        id - A unique ID for each instance
+        created_at - Time when an instance is created
+        updated_at - Time whenever an object is modified
+        """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ A string representation instances of the class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute with the current time"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returs a dictionary containing all keys/values of __dict__
+        of the instance
+        __class__ key is be added to the dictionary with the class name
+        of the object"""
+        attributes = self.__dict__
+        class_dict = {attr: value.isoformat() if attr ==
+                      'created_at' or attr == 'updated_at' else value for attr,
+                      value in attributes.items()}
+        class_dict['__class__'] = self.__class__.__name__
+        return class_dict
